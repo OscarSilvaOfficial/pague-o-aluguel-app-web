c256dcb47767dfd422784faf21436f10fcb3cb28
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-06-17 18:20:42 -0300

    .

---
+++ src/services/configs.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyAqF4727I2_oxvp33i9ZPtrhlGjYBI-TQ0',
+  authDomain: 'estudos-f954f.firebaseapp.com',
+  messagingSenderId: '117788654637',
+  projectId: 'estudos-f954f',
+  storageBucket: 'estudos-f954f.appspot.com',
+  appId: '1:117788654637:web:851973cb3de6eb21d70ca4',
+  measurementId: 'G-4BL9LS7G4D',
+};
